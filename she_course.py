b05279b43f75dbbd9f8fd1c0dfb1d9fac44a3fcf
Faris Faiz <u2000421@siswa.um.edu.my>
2023-02-22 23:25:02 +0800

    Modified for deploying purposes

--- she_course.py
+++ she_course.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-excel_file = ('C://Users//mfari//Documents//Python Codes//SHE Course Helper//SHE COURSES.xlsx')
+excel_file = ('SHE COURSES.xlsx')
 
 st.set_page_config(page_title="SHE Helper!",
                    page_icon=":book:",
@@ -48,6 +48,8 @@ else:
 ## MAINPAGE
 st.title(":book: SHE Course Helper!")
 st.subheader("By Faris Faiz and Faiqah Azman")
+link = '[Source Code](https://github.com/Faris-Faiz/SHE-Course-Helper)'
+st.markdown(link, unsafe_allow_html=True)
 
 vacant_courses = she_courses_cluster1['FULL'].value_counts()['F']
 
@@ -64,4 +66,5 @@ she_courses_cluster1.groupby(by=["FULL"]).sum()
 
 st.title("List of Available Courses:")
 st.subheader("Any -1 Values mean it's unknown, Registered means people have registered for the subject, Capacity is the capacity of the subject. \nIt's better to focus on the subjects that don't have any negative numbers!")
+df_selection.drop(columns=['FULL', 'CLUSTER'], axis=1, inplace=True)
 st.dataframe(df_selection)
